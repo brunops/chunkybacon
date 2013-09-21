7c79278b3067e661dc2800defde6582fc0f21cd0
Bruno Sanches <brunopsanches@gmail.com>
2013-09-21 11:40:40 -0700

    Add each method

--- chunkybacon.js
+++ chunkybacon.js
@@ -1,1 +1,10 @@
-alert("suuup")
+var CB = (function() {
+  return {
+    each: function(arr, callback) {
+      for (var i = 0; i < arr.length; ++i) {
+        arr[i] = callback(arr[i]);
+      }
+      return arr;
+    }
+  };
+})();
